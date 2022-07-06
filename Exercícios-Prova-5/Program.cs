3241cd8e0ee721a9078247028fffc139928a9e9c
Leo <Leo@DESKTOP-LU2I4O1>
2022-07-06 19:04:52 -0300

    Adicionar arquivos de projeto.

---
+++ Exercícios-Prova-5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercícios_Prova_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //5 - Fazer um programa para encontrar todos os números pares entre 1 e 100.
+            int n1 = 0;
+            while (n1 < 100)
+            {
+                n1 = n1 + 2;
+                Console.WriteLine(n1);
+            }
+            Console.ReadKey();
+        }
+    }
+}
